bf8e88655916a88efdf3e863e1be2c0f913e5d9e
Nicolò Curioni <nicolo.curioni96@gmail.com>
2023-10-22 19:20:16 +0200

    Initial Commit

---
+++ Connected/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Connected
+//
+//  Created by Nicolò Curioni on 22/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
